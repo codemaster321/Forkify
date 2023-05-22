02922b7c6eef94fd146a05847aa738913d2bb765
codemaster321 <shivendrashukla06@gmail.com>
2023-05-22 20:37:33 +0530

    Final push for the project, might add more updates later

---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,115 @@
+import icons from 'url:../../img/icons.svg';
+import View from './View';
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+
+  _message = 'You successfully added the recipe. Hurray!';
+  _window = document.querySelector('.add-recipe-window');
+
+  _overlay = document.querySelector('.overlay');
+
+  _btonOpen = document.querySelector('.nav__btn--add-recipe');
+
+  _btnClose = document.querySelector('.btn--close-modal');
+
+  constructor() {
+    super();
+    this._addHandlerShowWindow();
+    this._addHandlerHideWindow();
+  }
+
+  _generateMarkup() {
+    return ` <div class="upload__column">
+  <h3 class="upload__heading">Recipe data</h3>
+  <label>Title</label>
+  <input value="TEST123" required name="title" type="text" />
+  <label>URL</label>
+  <input value="TEST123" required name="sourceUrl" type="text" />
+  <label>Image URL</label>
+  <input value="TEST123" required name="image" type="text" />
+  <label>Publisher</label>
+  <input value="TEST123" required name="publisher" type="text" />
+  <label>Prep time</label>
+  <input value="23" required name="cookingTime" type="number" />
+  <label>Servings</label>
+  <input value="23" required name="servings" type="number" />
+</div>
+
+<div class="upload__column">
+  <h3 class="upload__heading">Ingredients</h3>
+  <label>Ingredient 1</label>
+  <input
+    value="0.5,kg,Rice"
+    type="text"
+    required
+    name="ingredient-1"
+    placeholder="Format: 'Quantity,Unit,Description'"
+  />
+  <label>Ingredient 2</label>
+  <input
+    value="1,,Avocado"
+    type="text"
+    name="ingredient-2"
+    placeholder="Format: 'Quantity,Unit,Description'"
+  />
+  <label>Ingredient 3</label>
+  <input
+    value=",,salt"
+    type="text"
+    name="ingredient-3"
+    placeholder="Format: 'Quantity,Unit,Description'"
+  />
+  <label>Ingredient 4</label>
+  <input
+    type="text"
+    name="ingredient-4"
+    placeholder="Format: 'Quantity,Unit,Description'"
+  />
+  <label>Ingredient 5</label>
+  <input
+    type="text"
+    name="ingredient-5"
+    placeholder="Format: 'Quantity,Unit,Description'"
+  />
+  <label>Ingredient 6</label>
+  <input
+    type="text"
+    name="ingredient-6"
+    placeholder="Format: 'Quantity,Unit,Description'"
+  />
+</div>
+
+<button class="btn upload__btn">
+  <svg>
+    <use href="${icons}#icon-upload-cloud"></use>
+  </svg>
+  <span>Upload</span>
+</button>`;
+  }
+
+  _toggleWindow() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
+  }
+
+  _addHandlerShowWindow() {
+    this._btonOpen.addEventListener('click', this._toggleWindow.bind(this));
+  }
+
+  _addHandlerHideWindow() {
+    this._btnClose.addEventListener('click', this._toggleWindow.bind(this));
+  }
+
+  _addHandlerUpload(handler) {
+    this._parentElement.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      const dataArr = [...new FormData(this)];
+      const data = Object.fromEntries(dataArr);
+      handler(data);
+    });
+  }
+}
+
+export default new AddRecipeView();
